5f6399f28b9d17dfe1095ff18a711ac3d22a53d3
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-02-17 07:53:49 +0630

    day1

---
+++ app.js
@@ -0,0 +1,4 @@
+var gettablinks = document.querySelectorAll('.tablinks');
+var getbtnclose = document.getElementsByClassName('btn-close');
+var gettabpanes = document.getElementsByClassName('tab-pane');
+
